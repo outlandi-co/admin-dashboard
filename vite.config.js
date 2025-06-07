25ad88fd2777ea190f124a3a22fe047d4ac4385c
outlandi-co <outlandico@gmail.com>
2025-06-07 03:13:51 -0700

    troubleshooting with redirects and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/admin/',  // <-- this tells Vite to prefix asset URLs with /admin/
+  base: '/admin/',  // IMPORTANT: tells Vite to prefix asset URLs with /admin/
   plugins: [react()],
-});
+})
