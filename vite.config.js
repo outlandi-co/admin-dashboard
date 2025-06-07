da4032d0b4aaec2454370cd9f7b17b6dd615afe9
outlandi-co <outlandico@gmail.com>
2025-06-07 02:26:06 -0700

    troubleshooting files

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/admin/', // 👈 sets correct path for assets when deployed
+  base: '/admin/', // 👈 sets correct path for all static assets when deployed to /admin
   plugins: [react()],
 })
