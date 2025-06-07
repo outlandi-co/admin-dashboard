dd4eac90fcfcbf4c0b01347d2be2e7e5cecb1e14
outlandi-co <outlandico@gmail.com>
2025-06-07 03:04:46 -0700

    troubleshooting with redirects and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/admin/', // 👈 sets correct path for all static assets when deployed to /admin
+  base: '/admin/', // <-- crucial for correct asset URLs
   plugins: [react()],
-})
+});
